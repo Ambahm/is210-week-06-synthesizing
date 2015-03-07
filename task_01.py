2d94502ba99e4b92d9d49c6e41d2fdd132c7f58a
gpsingh12 <gurpreet_1441@yahoo.com>
2015-03-07 04:10:04 +0000

    edit

---
+++ task_01.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Docstring. """
+
+import datetime
+CURDATE = None
+
+
+def get_current_date():
+    """ Function does not take any parameters
+        It returns today's day as date using
+        datetime.date.today()
+
+        Arg:
+            CURDATE(date): It is assigned the value of today's date.
+        Return:
+            Function returns todays day as date.
+    """
+
+    today = datetime.date.today()
+   # CURDATE = today
+    return today
