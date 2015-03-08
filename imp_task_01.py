8ef50c083adb110e8a098bd0e2f61c93927c7a64
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2015-03-08 21:52:00 +0000

    'Completed task 1, created test file, and ran all tests(passed.'

---
+++ imp_task_01.py
@@ -0,0 +1,8 @@
+#!/usr/env/python
+# -*- coding: utf-8 *-*
+"""This is a simple test file that outputs the value of CURDATE from
+task_01.py and should return a value of \'None\'."""
+
+import task_01
+
+print task_01.CURDATE
